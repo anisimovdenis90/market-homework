3d99c188346803e8f7527c34fece534627711b8c
Denis Anisimov <anisimov.denis.90@gmail.com>
2020-10-12 21:31:59 +0300

    Практическая работа №8

--- src/main/java/ru/geekbrains/markethomework/services/UserService.java
+++ src/main/java/ru/geekbrains/markethomework/services/UserService.java
@@ -32,9 +32,6 @@ public class UserService implements UserDetailsService {
     @Transactional
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = findByUsername(username);
-        if (user == null) {
-            throw new UsernameNotFoundException(String.format("User '%s' not found", username));
-        }
         return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), mapRolesToAuthorities(user.getRoles()));
     }
 
