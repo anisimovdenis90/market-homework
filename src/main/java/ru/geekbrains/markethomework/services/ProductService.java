c83a5e25d2ff02c1ec5f41db058fb4cb1af49eaa
Denis Anisimov <anisimov.denis.90@gmail.com>
2020-11-13 20:41:57 +0300

    Подготовка к практической работе №4, уровень 2

--- src/main/java/ru/geekbrains/markethomework/services/ProductService.java
+++ src/main/java/ru/geekbrains/markethomework/services/ProductService.java
@@ -7,8 +7,12 @@ import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import ru.geekbrains.markethomework.entities.Product;
 import ru.geekbrains.markethomework.repositories.ProductRepository;
+import ru.geekbrains.markethomework.soap.ProductSoap;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -17,6 +21,19 @@ public class ProductService {
 
     public Page<Product> findAllProducts(Specification<Product> spec, int page, int size) {
         return productRepository.findAll(spec, PageRequest.of(page, size));
+    }
+
+    public List<ProductSoap> findAllProducts() {
+        List<Product> products = productRepository.findAll();
+        List<ProductSoap> soapList = products.stream().map(p -> {
+            ProductSoap ps = new ProductSoap();
+            ps.setId(p.getId());
+            ps.setPrice(p.getPrice());
+            ps.setTitle(p.getTitle());
+            ps.setCategoryTitle(p.getCategory().getTitle());
+            return ps;
+        }).collect(Collectors.toList());
+        return soapList;
     }
 
     public Optional<Product> findProductById(Long id) {
